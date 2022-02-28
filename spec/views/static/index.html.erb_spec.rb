613d7b8164b45b50ea0471ad8ed743ff881fd09f
beitaz <beitaz@icloud.com>
2022-02-28 14:49:11 +0800

    add static test

--- spec/views/static/index.html.erb_spec.rb
+++ spec/views/static/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "static/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+  end
+
+  it "renders a list of employees" do
+    render
+    assert_select "p", text: "Find me in app/views/static/index.html.erb"
+  end
 end
