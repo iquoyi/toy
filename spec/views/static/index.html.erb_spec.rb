0246c2bc349776053283b10acaa3339427a75f11
beitaz <beitaz@icloud.com>
2022-02-28 15:13:29 +0800

    add guard notifier

--- spec/views/static/index.html.erb_spec.rb
+++ spec/views/static/index.html.erb_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "static/index.html.erb", type: :view do
-  before(:each) do
-  end
-
   it "renders a list of employees" do
     render
     assert_select "p", text: "Find me in app/views/static/index.html.erb"
