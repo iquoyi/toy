041d1b26ecddca182fbbc5c4be4ce3ca1e5ccc74
beitaz <beitaz@icloud.com>
2022-02-28 14:38:15 +0800

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+import "../stylesheets/application"
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -9,3 +11,9 @@ import "channels"
 
 Rails.start()
 Turbolinks.start()
+
+import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  // scripts
+})
