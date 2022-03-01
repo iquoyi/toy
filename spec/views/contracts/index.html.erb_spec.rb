e6b986bdf494e4359ac37f75cf2d2f99f1eb8cd9
beitaz <beitaz@icloud.com>
2022-03-01 12:50:08 +0800

    add test for views/contracts/index

--- spec/views/contracts/index.html.erb_spec.rb
+++ spec/views/contracts/index.html.erb_spec.rb
@@ -3,17 +3,22 @@ require 'rails_helper'
 RSpec.describe "contracts/index", type: :view do
   before do
     assign(:contracts, [
-             Contract.create!(
-               legal: "Legal"
+             Contract.create(
+               start_date: '2022-03-01',
+               end_date: '2022-03-01',
+               legal: "Legal_1"
              ),
-             Contract.create!(
-               legal: "Legal"
+             Contract.create(
+               start_date: '2022-03-01',
+               end_date: '2022-03-01',
+               legal: "Legal_2"
              )
            ])
   end
 
   it "renders a list of contracts" do
     render
-    assert_select "tr>td", text: "Legal".to_s, count: 2
+    assert_select "tr>td", text: "Legal_1"
+    assert_select "tr>td", text: "Legal_2"
   end
 end
