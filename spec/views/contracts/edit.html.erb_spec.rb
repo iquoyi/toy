fb1e5d7995140ed872ddbe30d4b9dc7ec6407c91
beitaz <beitaz@icloud.com>
2022-03-01 13:00:17 +0800

    update test for views/contracts

--- spec/views/contracts/edit.html.erb_spec.rb
+++ spec/views/contracts/edit.html.erb_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "contracts/edit", type: :view do
   before do
-    @contract = assign(:contract, Contract.create!(
+    @contract = assign(:contract, Contract.create(
+                                    start_date: '2022-03-01',
+                                    end_date: '2022-03-02',
                                     legal: "MyString"
                                   ))
   end
@@ -10,8 +12,29 @@ RSpec.describe "contracts/edit", type: :view do
   it "renders the edit contract form" do
     render
 
-    assert_select "form[action=?][method=?]", contract_path(@contract), "post" do
+    assert_select "form[action=?][method=?]", contract_path(@contract.id), "post" do
+      assert_select "input[name=_method][value=?]", "patch"
+
+      assert_select "input[type=date][name=?]", "contract[start_date]"
+      assert_select "input[type=date][value=?]", "2022-03-01"
+
+      assert_select "input[type=date][name=?]", "contract[end_date]"
+      assert_select "input[type=date][value=?]", "2022-03-02"
+
       assert_select "input[name=?]", "contract[legal]"
+      assert_select "input[type=text][value=?]", "MyString"
     end
+  end
+
+  it 'renders include show link' do
+    render
+    expect(rendered).to include('Show')
+    assert_select("a[href=?]", contract_path(@contract.id))
+  end
+
+  it 'renders include back link' do
+    render
+    expect(rendered).to include('Back')
+    assert_select("a[href=?]", contracts_path)
   end
 end
--- spec/views/contracts/new.html.erb_spec.rb
+++ spec/views/contracts/new.html.erb_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "contracts/new", type: :view do
   before do
     assign(:contract, Contract.new(
+                        start_date: '2022-03-01',
+                        end_date: '2022-03-01',
                         legal: "MyString"
                       ))
   end
@@ -11,7 +13,15 @@ RSpec.describe "contracts/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", contracts_path, "post" do
+      assert_select "input[type=date][name=?]", "contract[start_date]"
+      assert_select "input[type=date][name=?]", "contract[end_date]"
       assert_select "input[name=?]", "contract[legal]"
     end
+  end
+
+  it 'renders include back link' do
+    render
+    expect(rendered).to include('Back')
+    assert_select("a[href=?]", contracts_path)
   end
 end
