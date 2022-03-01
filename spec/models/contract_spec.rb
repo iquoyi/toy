b2b134ff0c2a74071a3682ed7aa409fd6291898f
beitaz <beitaz@icloud.com>
2022-03-01 22:31:09 +0800

    update contract test

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,56 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-  let(:valid_attributes) do
-    {
-      start_date: '2022-03-01',
-      end_date: '2022-03-02',
-      legal: 'Legal'
-    }
+  before do
+    Timecop.freeze 2009, 11, 28
   end
 
-  let(:invalid_attributes) do
-    {
-      start_date: '2022-03-01',
-      end_date: '',
-      legal: 'Legal'
-    }
+  after do
+    Timecop.return
   end
 
-  context 'with valid parameters' do
-    it 'create a contract' do
-      expect do
-        described_class.create(valid_attributes)
-      end.to change(described_class, :count).by(1)
-    end
-
-    it 'update a contract' do
-      contract = described_class.create(valid_attributes)
-      contract.update(legal: 'New Legal')
-      contract.reload
-      expect(contract.legal).to eq('New Legal')
-    end
-  end
-
-  context 'with invalid parameters' do
-    it 'create a contract' do
-      expect do
-        described_class.create(invalid_attributes)
-      end.to raise_error("end_date can't be blank")
-    end
-
-    it 'update a contract' do
-      contract = described_class.create(valid_attributes)
-      expect do
-        contract.update(start_date: nil)
-      end.to raise_error("start_date can't be blank")
-    end
-  end
-
-  it 'destroy a contract' do
-    contract = described_class.create(valid_attributes)
+  it "checks version class is given" do
     expect do
-      contract.delete
-    end.to change(described_class, :count).by(-1)
+      described_class.plugin :bitemporal
+    end.to raise_error Sequel::Error, "please specify version class to use for bitemporal plugin"
+  end
+
+  it "defines current_versions_dataset" do
+    described_class.current_versions_dataset.delete
+    described_class.new
+                   .update_attributes(start_date: Time.zone.today - 1, end_date: Time.zone.today + 1)
+                   .update_attributes(legal: "King Size")
+    versions = described_class.current_versions_dataset.all
+    expect(versions.size).to eq(1)
+    expect(versions[0].legal).to eq("King Size")
+  end
+
+  it "don't create any new version without change" do
+    employee = described_class.new
+    employee.update_attributes start_date: Time.zone.today - 1, end_date: Time.zone.today + 1
+    employee.update_attributes end_date: Time.zone.today + 1
+    employee.update_attributes start_date: Time.zone.today - 1, end_date: Time.zone.today + 1
+    expect(employee).to have_versions %(
+      | start_date | end_date   | created_at | expired_at | valid_from | valid_to | current |
+      | 2009-11-27 | 2009-11-29 | 2009-11-28 |            | 2009-11-28 | MAX DATE | true    |
+    )
   end
 end
--- spec/models/contract_version_spec.rb
+++ spec/models/contract_version_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe ContractVersion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'normal test' do
+    let(:valid_attributes) do
+      {
+        start_date: '2022-03-01',
+        end_date: '2022-03-01',
+        legal: 'Legal'
+      }
+    end
+
+    let(:invalid_attributes) do
+      {
+        start_date: '',
+        end_date: '2022-03-01',
+        legal: 'Legal'
+      }
+    end
+
+    context 'with valid parameters' do
+      it 'create a contract' do
+        expect do
+          described_class.create(valid_attributes)
+        end.to change(described_class, :count).by(1)
+      end
+
+      it 'update a contract' do
+        contract = described_class.create(valid_attributes)
+        contract.update(legal: 'New Legal')
+        contract.reload
+        expect(contract.legal).to eq('New Legal')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'create a contract' do
+        expect do
+          described_class.create(invalid_attributes)
+        end.to raise_error("start_date can't be blank")
+      end
+
+      it 'update a contract' do
+        contract = described_class.create(valid_attributes)
+        expect do
+          contract.update(start_date: nil)
+        end.to raise_error("start_date can't be blank")
+      end
+    end
+
+    it 'destroy a contract' do
+      contract = described_class.create(valid_attributes)
+      expect do
+        contract.delete
+      end.to change(described_class, :count).by(-1)
+    end
+  end
+
+  # rubocop:disable Layout/LineLength
+  it "checks required columns are present" do
+    expect do
+      described_class.plugin :bitemporal, version_class: Contract
+    end.to raise_error Sequel::Error, "bitemporal plugin requires the following missing columns on version class: master_id, valid_from, valid_to, created_at, expired_at"
+  end
+  # rubocop:enable Layout/LineLength
 end
