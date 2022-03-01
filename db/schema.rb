a385d505fe36719349299e02dd03af38b58c9b00
beitaz <beitaz@icloud.com>
2022-03-01 22:11:02 +0800

    update contract to bitemporal

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,19 @@
 Sequel.migration do
   change do
-    create_table(:contracts) do
+    create_table(:contract_versions) do
       primary_key :id, :type=>"INTEGER"
+      column :master_id, "INTEGER"
+      column :valid_from, "date"
+      column :valid_to, "date"
       column :start_date, "date"
       column :end_date, "date"
-      column :legal, "varchar(255)"
+      column :legal, "TEXT"
+      column :created_at, "date"
+      column :expired_at, "date"
+    end
+    
+    create_table(:contracts) do
+      primary_key :id, :type=>"INTEGER"
     end
     
     create_table(:employee_versions) do
@@ -37,5 +46,7 @@ end
 self << "INSERT INTO `schema_migrations` (`filename`) VALUES ('20220301043314_create_contracts.rb')"
 self << "INSERT INTO `schema_migrations` (`filename`) VALUES ('20220301075542_create_employee_versions.rb')"
 self << "INSERT INTO `schema_migrations` (`filename`) VALUES ('20220301075941_remove_unused_columns_from_employees.rb')"
+self << "INSERT INTO `schema_migrations` (`filename`) VALUES ('20220301140748_create_contract_versions.rb')"
+self << "INSERT INTO `schema_migrations` (`filename`) VALUES ('20220301140810_remove_unused_columns_from_contracts.rb')"
                 end
               end
