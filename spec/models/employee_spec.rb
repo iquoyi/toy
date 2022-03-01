3f0b0e0782295663925f4612144e33634915cc8d
beitaz <beitaz@icloud.com>
2022-03-01 17:03:19 +0800

    temp commit

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending
+  it "checks version class is given" do
+    expect do
+      described_class.plugin :bitemporal
+    end.to raise_error Sequel::Error, "please specify version class to use for bitemporal plugin"
+  end
+
+  # it "defines current_versions_dataset" do
+  #   described_class.new
+  #                  .update_attributes(frist_name: "Single Standard")
+  #                  .update_attributes(first_name: "King Size")
+  #   versions = described_class.current_versions_dataset.all
+  #   expect(versions.size).to eq(1)
+  #   expect(versions[0].first_name).to eq("King Size")
+  # end
 end
--- spec/models/employee_version_spec.rb
+++ spec/models/employee_version_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe EmployeeVersion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "checks required columns are present" do
+    expect do
+      described_class.plugin :bitemporal, version_class: Employee
+    end.to raise_error Sequel::Error, "bitemporal plugin requires the following missing columns \
+                                      on version class: master_id, valid_from, valid_to, created_at, expired_at"
+  end
 end
