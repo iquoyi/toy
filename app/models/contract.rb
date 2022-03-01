ae7800710ab2fbd154407f4c653d4c99e5fbf303
beitaz <beitaz@icloud.com>
2022-03-01 13:51:37 +0800

    add contract model test

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,7 +1,7 @@
 class Contract < Sequel::Model
   def validate
     super
-    errors.add(:start_date, "can't be empty") if start_date.blank?
-    errors.add(:end_date, "can't be empty") if end_date.blank?
+    errors.add(:start_date, "can't be blank") if start_date.blank?
+    errors.add(:end_date, "can't be blank") if end_date.blank?
   end
 end
