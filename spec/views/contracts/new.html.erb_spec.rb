08e27e87dd2c48df7576a6179f155b91064ee5eb
beitaz <beitaz@icloud.com>
2022-03-03 09:31:23 +0800

    update contracts tests after change route

--- spec/views/contracts/new.html.erb_spec.rb
+++ spec/views/contracts/new.html.erb_spec.rb
@@ -2,17 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "contracts/new", type: :view do
   before do
-    assign(:contract, Contract.new(
-                        start_date: '2022-03-01',
-                        end_date: '2022-03-01',
-                        legal: "MyString"
-                      ))
+    @employee = Employee.create(first_name: 'First', last_name: 'Last')
+    @contract = Contract.new(employee_id: @employee.id)
   end
 
   it "renders new contract form" do
     render
 
-    assert_select "form[action=?][method=?]", contracts_path, "post" do
+    assert_select "form[action=?][method=?]", employee_contracts_path(@employee.id), "post" do
       assert_select "input[type=date][name=?]", "contract[start_date]"
       assert_select "input[type=date][name=?]", "contract[end_date]"
       assert_select "input[name=?]", "contract[legal]"
@@ -22,6 +19,6 @@ RSpec.describe "contracts/new", type: :view do
   it 'renders include back link' do
     render
     expect(rendered).to include('Back')
-    assert_select("a[href=?]", contracts_path)
+    assert_select("a[href=?]", employee_path(@employee.id))
   end
 end
