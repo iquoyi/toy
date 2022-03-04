acc1bc459bdab75a3eda3ac86ccd40d8113468c1
beitaz <beitaz@icloud.com>
2022-03-04 12:07:05 +0800

    update test for contracts/index after add date validates

--- spec/views/contracts/index.html.erb_spec.rb
+++ spec/views/contracts/index.html.erb_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "contracts/index", type: :view do
   before do
     @employee = Employee.create(first_name: 'First', last_name: 'Last')
     @contract1 = Contract.new(employee_id: @employee.id)
-    @contract1.update_attributes(start_date: "2022-03-01", end_date: "2022-03-01", legal: "Legal_1")
+    @contract1.update_attributes(start_date: "2022-03-01", end_date: "2022-03-02", legal: "Legal_1")
 
     @contract2 = Contract.new(employee_id: @employee.id)
-    @contract2.update_attributes(start_date: "2022-03-02", end_date: "2022-03-02", legal: "Legal_2")
+    @contract2.update_attributes(start_date: "2022-03-03", end_date: "2022-03-04", legal: "Legal_2")
 
     @contracts = [@contract1, @contract2]
   end
